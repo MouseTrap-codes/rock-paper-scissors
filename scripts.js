5f347e360c4afbba8895ae2c08af76e0a827f137
MouseTrap-codes <mousetrapcodes@gmail.com>
2025-06-19 00:38:34 -0500

    Create logic to play a round
    Now we can play a single round of our rock-paper-
    scissors game.

--- scripts.js
+++ scripts.js
@@ -14,3 +14,29 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase(); // make case insensitive
+
+    if (humanChoice === computerChoice) {
+        console.log(`both you and the bot played ${humanChoice}!. The round was a tie!`);
+    } else if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            console.log(`you lose -> ${computerChoice} beats ${humanChoice} :(`)
+        } else if (computerChoice === "scissors") {
+            console.log(`you win! ${humanChoice} beats ${computerChoice}!!!`);
+        }
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            console.log(`you lose -> ${computerChoice} beats ${humanChoice} :(`)
+        } else if (computerChoice === "paper") {            
+            console.log(`you win! ${humanChoice} beats ${computerChoice}!!!`);
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "scissors") {
+            console.log(`you lose -> ${computerChoice} beats ${humanChoice} :(`)
+        } else if (computerChoice === "rock") {
+            console.log(`you win! ${humanChoice} beats ${computerChoice}!!!`);
+        }
+    }
+}
