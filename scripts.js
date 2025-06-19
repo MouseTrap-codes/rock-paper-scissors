b6587f56ba6d1f5f0f6026770056f70464aebac5
MouseTrap-codes <mousetrapcodes@gmail.com>
2025-06-19 00:51:35 -0500

    Finish gameplay loop
    The game now plays for five rounds and announces
    the scores and winners at the end.

--- scripts.js
+++ scripts.js
@@ -12,31 +12,65 @@ function getHumanChoice() {
     return prompt("Please enter your play (rock, paper, or scissors):");
 }
 
-let humanScore = 0;
-let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
     humanChoice = humanChoice.toLowerCase(); // make case insensitive
 
     if (humanChoice === computerChoice) {
-        console.log(`both you and the bot played ${humanChoice}!. The round was a tie!`);
+        alert(`both you and the bot played ${humanChoice}! 
+            the round was a tie!`);
+        return -1;
     } else if (humanChoice === "rock") {
         if (computerChoice === "paper") {
-            console.log(`you lose -> ${computerChoice} beats ${humanChoice} :(`)
+            alert(`you lose -> ${computerChoice} beats ${humanChoice} :(`)
+            return 0;
         } else if (computerChoice === "scissors") {
-            console.log(`you win! ${humanChoice} beats ${computerChoice}!!!`);
+            alert(`you win! ${humanChoice} beats ${computerChoice}!!!`);
+            return 1;
         }
     } else if (humanChoice === "scissors") {
         if (computerChoice === "rock") {
-            console.log(`you lose -> ${computerChoice} beats ${humanChoice} :(`)
+            alert(`you lose -> ${computerChoice} beats ${humanChoice} :(`)
+            return 0;
         } else if (computerChoice === "paper") {            
-            console.log(`you win! ${humanChoice} beats ${computerChoice}!!!`);
+            alert(`you win! ${humanChoice} beats ${computerChoice}!!!`);
+            return 1;
         }
     } else if (humanChoice === "paper") {
         if (computerChoice === "scissors") {
-            console.log(`you lose -> ${computerChoice} beats ${humanChoice} :(`)
+            alert(`you lose -> ${computerChoice} beats ${humanChoice} :(`)
+            return 0;
         } else if (computerChoice === "rock") {
-            console.log(`you win! ${humanChoice} beats ${computerChoice}!!!`);
+            alert(`you win! ${humanChoice} beats ${computerChoice}!!!`);
+            return 1;
         }
     }
 }
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 4; i++) {
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+
+        let result = playRound(humanSelection, computerSelection);
+        
+        if (result === 0) {
+            computerScore++;
+        } else if (result === 1) {
+            humanScore++;
+        } 
+    }
+
+    if (humanScore > computerScore) {
+        alert(`you won with a score of ${humanScore} - ${computerScore}!!!`);
+    } else if (computerScore > humanScore) {
+        alert(`you lost ${humanScore} - ${computerScore} :(`);
+    } else {
+        alert("it was a tie :/");
+    }  
+}
+
+playGame();
